1a01e80b6e3910491b0c339ca5813f2786e880f8
diamant1991 <diamant1988@mail.ru>
2015-07-16 23:26:48 +0300

    first files

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	$.fn.equivalent = function (){
+        var $blocks = $(this),
+            maxH    = $blocks.eq(0).height(); 
+        $blocks.each(function(){
+            maxH = ( $(this).height() > maxH ) ? $(this).height() : maxH;
+        });
+
+        $blocks.height(maxH); 
+    }
+    $('.height-side').equivalent();
+});
